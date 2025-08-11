5731ff7877605d6cf02f3405ce847a5fd5327e83
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-08-11 18:25:25 +0300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import WebVM from '$lib/WebVM.svelte';
+	import * as configObj from '/config_terminal.js';
+</script>
+
+<svelte:head>
+	<title>Portfolio VM - Linux Terminal</title>
+	<meta name="description" content="Interactive Linux environment to showcase portfolio applications" />
+</svelte:head>
+
+<WebVM {configObj} cacheId="portfolio_vm" />
